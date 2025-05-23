04548499404cd2bca6276d9d19f10519add10787
dbartolai <drakeab2@illinois.edu>
2025-05-22 19:20:56 -0500

    file structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Login from './Login'
 import Register from './Register'
-import LandingPage from './LandingPage'
+import LandingPage from './LandingPage/LandingPage'
 
 function App() {
 
